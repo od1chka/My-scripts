2ab5d1b56a9ce373a9f737b9d2b0d30f11258c1a
od1chka <Vod1chka125@gmail.com>
2024-11-09 17:37:20 +0500

    Hello world
    zzzzz

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод в консоль Hello world
+        print("Hello, World");
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hello, World");
+    }
+}
