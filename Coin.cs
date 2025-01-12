ca00f512bf96821f8d9fa93ea1d3da561a481ad9
od1chka <Vod1chka125@gmail.com>
2025-01-12 18:20:37 +0500

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+
+        Player player = other.GetComponent<Player>();
+
+        //Количество монеток обновляется
+        player.CollectCoins();
+
+        //Монетка, которую собрали, уничтожается
+        Destroy(gameObject);
+    }
+}
+
+
+
